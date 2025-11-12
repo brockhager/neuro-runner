ba2e588faf9e32b2066bcb064f5adb50a2df09e0
Brock <brockhager@users.noreply.github.com>
2025-11-11 18:54:30 -0700

    Initial scaffolding with TypeScript node setup

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// Main entry point for NeuroSwarm runner
+
+console.log("NeuroSwarm Runner starting...");
+
+// TODO: Initialize node based on configuration
+// TODO: Connect to Solana network
+// TODO: Start appropriate services (validator, indexer, gateway)
